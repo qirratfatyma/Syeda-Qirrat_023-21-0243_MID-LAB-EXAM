80717720662dce961e664fe04fc61ed5c9b9bc75
qirratfatyma <syedaqirratfatima@gmail.com>
2023-10-23 15:56:00 +0500

    Updated

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function submitRequest() {
     alert('Request submitted!');
   }
-  
+var button = document.createElement("button");
+
+button.setAttribute("id", "myButton");
+button.textContent = "Click Me";
+
+document.body.appendChild(button);
